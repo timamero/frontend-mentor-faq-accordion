167cad4b168ef2bb3b962b90b46aef1b3353d51b
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-03-28 13:19:43 -0700

    style: Set bg position and z-index

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div>
       <Layout>
-        <div>
-          <h1>test content</h1>
+        <div style={{ zIndex: 1, position: 'relative' }}>
+          <h1 style={{ margin: 0 }}>test content</h1>
           <p> test content </p>
         </div>
       </Layout>
