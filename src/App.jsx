d156303993a80157a91334c2eab7a0928aa7bcd9
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-03-29 13:33:26 -0700

    Create container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import Container from './components/Container';
 import Layout from './components/Layout';
 
 function App() {
   return (
     <div>
       <Layout>
-        <div style={{ zIndex: 1, position: 'relative' }}>
+        <Container>
+          <h1>FAQs</h1>
+          <p>What is Frontend Mentor</p>
+        </Container>
+        {/* <div style={{ zIndex: 1, position: 'relative' }}>
           <h1 style={{ margin: 0 }}>test content</h1>
           <p> test content </p>
-        </div>
+        </div> */}
       </Layout>
     </div>
   );
