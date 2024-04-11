d5595142f29ca3841b0dfdd0dc9a306df8fdb697
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-04-11 15:23:59 -0700

    style: Add minus icon and align

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,5 +1,11 @@
 import styles from './Accordion.module.css';
+import iconMinus from '../../assets/images/icon-minus.svg';
 
 export default function AccordionItem({ children }) {
-  return <div className={styles.accordionItem}>{children}</div>;
+  return (
+    <div className={styles.accordionItem}>
+      <p>{children}</p>
+      <img src={iconMinus} height="32px" width="32px" />
+    </div>
+  );
 }
