a49d545e7d2a2332a6ce2d5257ea810b37e546cb
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-04-11 15:04:08 -0700

    feat: Render list with faq data

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,18 @@ import Layout from './layout/Layout';
 import { faqsData } from './data';
 
 function App() {
+  const accordionGroup = faqsData.map((obj) => (
+    <div key={obj.id}>
+      <AccordionItem>{obj.question}</AccordionItem>
+      <AccordionDetails>{obj.answer}</AccordionDetails>
+    </div>
+  ));
   console.log('data', faqsData);
   return (
     <Layout>
       <Container>
         <Card title="FAQs">
-          <Accordion>
-            <AccordionItem>Item placeholder</AccordionItem>
-            <AccordionDetails>Details placeholder</AccordionDetails>
-          </Accordion>
+          <Accordion>{accordionGroup}</Accordion>
         </Card>
       </Container>
     </Layout>
