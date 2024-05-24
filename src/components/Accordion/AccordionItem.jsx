5aa18400f0ffd68fd9307db33ce53b355c9209e0
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-05-24 00:54:13 -0700

    refactor: Wrap button in h2

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -4,9 +4,11 @@ import iconPlus from '../../assets/images/icon-plus.svg';
 
 export default function AccordionItem({ isActive, onClick, id, children }) {
   return (
-    <button onClick={() => onClick(id)} className={styles.accordionItem}>
-      <p>{children}</p>
-      <img src={isActive ? iconMinus : iconPlus} height="32px" width="32px" />
-    </button>
+    <h2>
+      <button onClick={() => onClick(id)} className={styles.accordionItem}>
+        <p>{children}</p>
+        <img src={isActive ? iconMinus : iconPlus} height="32px" width="32px" />
+      </button>
+    </h2>
   );
 }
