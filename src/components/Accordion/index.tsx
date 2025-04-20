c06b54a354c0b0fc5966c0cea49b4e46ea3dd8bd
Fatima Camero <54559570+timamero@users.noreply.github.com>
2025-04-19 22:15:25 -0700

    Remove disabled code

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,4 +1,2 @@
 export { default as Accordion } from './Accordion.tsx';
-// export { default as AccordionItem } from './AccordionItem.tsx';
-// export { default as AccordionDetails } from './AccordionDetails.tsx';
 export { default as AccordionGroup } from './AccordionGroup.tsx';
